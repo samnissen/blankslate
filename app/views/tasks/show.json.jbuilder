c9802d4d168493fefc26c026f5e6cfe470bd4b38
Prelang Builder <builder@prelang.com>
2015-02-10 07:23:27 +0000

    Generated scaffolding for model 'Task' via Rails

---
+++ app/views/tasks/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @task, :id, :name, :description, :code, :variables, :task_type_id, :created_at, :updated_at
---
+++ spec/views/tasks/index.html.erb_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe "tasks/index", type: :view do
+  before(:each) do
+    assign(:tasks, [
+      Task.create!(
+        :name => "Name",
+        :description => "Description",
+        :code => "MyText",
+        :variables => "MyText",
+        :task_type => nil
+      ),
+      Task.create!(
+        :name => "Name",
+        :description => "Description",
+        :code => "MyText",
+        :variables => "MyText",
+        :task_type => nil
+      )
+    ])
+  end
+
+  it "renders a list of tasks" do
+    render
+    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    assert_select "tr>td", :text => "Description".to_s, :count => 2
+    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+    assert_select "tr>td", :text => nil.to_s, :count => 2
+  end
+end
